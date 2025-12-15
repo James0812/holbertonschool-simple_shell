305f65ddba6ed585b27caa9f7ba9d397b25c16b7
James0812 <james.roussel008@gmail.com>
2025-12-15 15:07:53 +0100

    Refactor code to pass Betty style checks

---
+++ 1-betty-checker.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * betty_main - Entry point for Betty checker
+ *
+ * Return: Always 0
+ */
+int betty_main(void)
+{
+    printf("Hello, World!\n");
+    return (0);
+}
+
