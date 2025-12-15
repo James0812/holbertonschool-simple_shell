a262a01030526e6c0c4ffd40672eff7a17c3210b
James0812 <james.roussel008@gmail.com>
2025-12-15 14:50:36 +0100

    Implement basic UNIX command line interpreter

--- simple_shell.c
+++ simple_shell.c
@@ -7,68 +7,98 @@
 extern char **environ;
 
 /**
+ * trim - Removes leading and trailing spaces from a string
+ * @str: String to process
+ *
+ * Return: Pointer to the trimmed string
+ */
+char *trim(char *str)
+{
+    char *end;
+
+    /* Trim leading spaces */
+    while (*str == ' ' || *str == '\t')
+        str++;
+
+    if (*str == 0) /* empty string */
+        return str;
+
+    /* Trim trailing spaces */
+    end = str + strlen(str) - 1;
+    while (end > str && (*end == ' ' || *end == '\t'))
+        end--;
+
+    *(end + 1) = '\0';
+    return str;
+}
+
+/**
  * main - Simple UNIX command interpreter
  *
  * Return: 0 on success
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	pid_t pid;
-	int status;
-	char *args[2];
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
+    char *args[2];
 
-	while (1)
-	{
-		/* Display prompt only in interactive mode */
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "($) ", 4);
+    while (1)
+    {
+        /* Display prompt only in interactive mode */
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "($) ", 4);
 
-		/* Read input */
-		nread = getline(&line, &len, stdin);
-		if (nread == -1) /* EOF (Ctrl+D) */
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			free(line);
-			exit(EXIT_SUCCESS);
-		}
+        /* Read command line */
+        nread = getline(&line, &len, stdin);
+        if (nread == -1) /* EOF (Ctrl+D) */
+        {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
 
-		/* Remove newline */
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+        /* Remove newline */
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
 
-		/* Fork process */
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			free(line);
-			exit(EXIT_FAILURE);
-		}
+        /* Trim spaces */
+        args[0] = trim(line);
+        args[1] = NULL;
 
-		if (pid == 0)
-		{
-			/* Child process */
-			args[0] = line;
-			args[1] = NULL;
+        if (args[0][0] == '\0') /* empty line */
+            continue;
 
-			if (execve(line, args, environ) == -1)
-			{
-				perror(line);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			/* Parent process */
-			waitpid(pid, &status, 0);
-		}
-	}
+        /* Fork child process */
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            free(line);
+            exit(EXIT_FAILURE);
+        }
 
-	free(line);
-	return (0);
+        if (pid == 0)
+        {
+            /* Child executes the command */
+            if (execve(args[0], args, environ) == -1)
+            {
+                perror(args[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            /* Parent waits for child to finish */
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    free(line);
+    return (0);
 }
 
