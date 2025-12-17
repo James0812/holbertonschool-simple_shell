252f99f5696fa106f70d69151c947cfaf2c0e99f
James0812 <james.roussel008@gmail.com>
2025-12-17 15:25:09 +0100

    Implement basic UNIX shell with built-ins and PATH handling

--- simple_shell.c
+++ simple_shell.c
@@ -16,15 +16,15 @@ extern char **environ;
  */
 char *get_path(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while (environ[i])
-    {
-        if (strncmp(environ[i], "PATH=", 5) == 0)
-            return (environ[i] + 5);
-        i++;
-    }
-    return NULL;
+	while (environ[i])
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+			return (environ[i] + 5);
+		i++;
+	}
+	return NULL;
 }
 
 /**
@@ -35,42 +35,42 @@ char *get_path(void)
  */
 char *find_command(char *command)
 {
-    char *path, *path_copy, *dir;
-    static char full_path[MAX_PATH];
+	char *path, *path_copy, *dir;
+	static char full_path[MAX_PATH];
 
-    if (command == NULL)
-        return NULL;
+	if (command == NULL)
+		return NULL;
 
-    /* If command already contains '/' */
-    if (strchr(command, '/') != NULL)
-    {
-        if (access(command, X_OK) == 0)
-            return command;
-        return NULL;
-    }
+	/* If command already contains '/' */
+	if (strchr(command, '/') != NULL)
+	{
+		if (access(command, X_OK) == 0)
+			return command;
+		return NULL;
+	}
 
-    path = get_path();
-    if (path == NULL)
-        return NULL;
+	path = get_path();
+	if (path == NULL || path[0] == '\0')
+		return NULL;
 
-    path_copy = strdup(path);
-    if (path_copy == NULL)
-        return NULL;
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+		return NULL;
 
-    dir = strtok(path_copy, ":");
-    while (dir != NULL)
-    {
-        snprintf(full_path, MAX_PATH, "%s/%s", dir, command);
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return full_path;
-        }
-        dir = strtok(NULL, ":");
-    }
+	dir = strtok(path_copy, ":");
+	while (dir != NULL)
+	{
+		snprintf(full_path, MAX_PATH, "%s/%s", dir, command);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return full_path;
+		}
+		dir = strtok(NULL, ":");
+	}
 
-    free(path_copy);
-    return NULL;
+	free(path_copy);
+	return NULL;
 }
 
 /**
@@ -80,67 +80,70 @@ char *find_command(char *command)
  */
 int main(void)
 {
-    char *line = NULL, *args[MAX_TOKENS];
-    size_t len = 0;
-    ssize_t read;
-    pid_t pid;
-    int status;
-    char *cmd_path;
-    int i;
-    int interactive;
+	char *line = NULL, *args[MAX_TOKENS];
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	int status;
+	char *cmd_path;
+	int i;
+	int interactive;
+	int line_count = 0; /* Used for error message numbering */
 
-    interactive = isatty(STDIN_FILENO);
+	interactive = isatty(STDIN_FILENO);
 
-    while (1)
-    {
-        if (interactive)
-            write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+		line_count++;
 
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-        {
-            if (interactive)
-                write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
+		if (interactive)
+			write(STDOUT_FILENO, "$ ", 2);
 
-        /* Remove newline */
-        line[strcspn(line, "\n")] = '\0';
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			if (interactive)
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
 
-        /* Tokenization */
-        i = 0;
-        args[i] = strtok(line, " ");
-        while (args[i] != NULL && i < MAX_TOKENS - 1)
-        {
-            i++;
-            args[i] = strtok(NULL, " ");
-        }
-        args[i] = NULL;
+		/* Remove newline */
+		line[strcspn(line, "\n")] = '\0';
 
-        /* Empty input */
-        if (args[0] == NULL)
-            continue;
+		/* Tokenization */
+		i = 0;
+		args[i] = strtok(line, " ");
+		while (args[i] != NULL && i < MAX_TOKENS - 1)
+		{
+			i++;
+			args[i] = strtok(NULL, " ");
+		}
+		args[i] = NULL;
 
-        cmd_path = find_command(args[0]);
-        if (cmd_path == NULL)
-        {
-            write(STDERR_FILENO, "command not found\n", 18);
-            continue;
-        }
+		/* Empty input */
+		if (args[0] == NULL)
+			continue;
 
-        pid = fork();
-        if (pid == 0)
-        {
-            if (execve(cmd_path, args, environ) == -1)
-                exit(EXIT_FAILURE);
-        }
-        else if (pid > 0)
-        {
-            wait(&status);
-        }
-    }
+		cmd_path = find_command(args[0]);
+		if (cmd_path == NULL)
+		{
+			dprintf(STDERR_FILENO, "./hsh: %d: %s: not found\n", line_count, args[0]);
+			continue;
+		}
 
-    free(line);
-    return 0;
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(cmd_path, args, environ) == -1)
+				exit(127);
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+		}
+	}
+
+	free(line);
+	return (0);
 }
 
