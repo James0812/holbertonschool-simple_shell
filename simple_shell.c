62cea9d81e19e71f8364351d43fdaa4643fc084d
James0812 <james.roussel008@gmail.com>
2025-12-19 09:59:33 +0100

    change code test

--- simple_shell.c
+++ simple_shell.c
@@ -8,187 +8,143 @@
 extern char **environ;
 
 /**
- * find_command_in_path - Searches for command in PATH directories
+ * find_in_path - find command in PATH
  * @cmd: command name
- * Return: full path if found, else NULL
+ *
+ * Return: full path or NULL
  */
-char *find_command_in_path(char *cmd)
+char *find_in_path(char *cmd)
 {
-    char *path_env, *path_copy, *dir, *full_path;
-    struct stat st;
+	char *path, *path_copy, *dir;
+	static char full_path[1024];
+	struct stat st;
+	int i;
 
-    if (strchr(cmd, '/'))
-    {
-        if (stat(cmd, &st) == 0)
-            return strdup(cmd);
-        return NULL;
-    }
+	for (i = 0; environ[i]; i++)
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+		{
+			path = environ[i] + 5;
+			break;
+		}
+	}
+	if (!environ[i] || !path || path[0] == '\0')
+		return (NULL);
 
-    path_env = NULL;
-    {
-        int i = 0;
-        while (environ[i])
-        {
-            if (strncmp(environ[i], "PATH=", 5) == 0)
-            {
-                path_env = environ[i] + 5;
-                break;
-            }
-            i++;
-        }
-    }
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
 
-    if (!path_env || path_env[0] == '\0')
-        return NULL;
-
-    path_copy = strdup(path_env);
-    if (!path_copy)
-        return NULL;
-
-    dir = strtok(path_copy, ":");
-    while (dir)
-    {
-        full_path = malloc(strlen(dir) + strlen(cmd) + 2);
-        if (!full_path)
-        {
-            free(path_copy);
-            return NULL;
-        }
-
-        sprintf(full_path, "%s/%s", dir, cmd);
-        if (stat(full_path, &st) == 0)
-        {
-            free(path_copy);
-            return full_path;
-        }
-
-        free(full_path);
-        dir = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return NULL;
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+		snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
+		if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
 }
 
 /**
- * main - Simple UNIX command interpreter (0.4)
+ * main - Simple UNIX command interpreter
+ *
  * Return: 0 on success
  */
-int main(void)
+int main(int argc, char **argv)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t pid;
-    int status = 0;
+	char *line = NULL, *cmd_path;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	int status, last_status = 0;
+	unsigned int line_number = 0;
+	char *tokens[100];
+	int i;
 
-    while (1)
-    {
-        char *tokens[100];
-        int i;
-        char *cmd_path;
+	(void)argc;
 
-        if (isatty(STDIN_FILENO))
-        {
-            write(STDOUT_FILENO, "($) ", 4);
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("($) "), fflush(stdout);
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            free(line);
-            exit(status);
-        }
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			free(line);
+			exit(last_status);
+		}
 
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+		line_number++;
 
-        /* Skip empty lines */
-        {
-            int only_spaces = 1;
-            for (i = 0; line[i]; i++)
-            {
-                if (line[i] != ' ' && line[i] != '\t')
-                {
-                    only_spaces = 0;
-                    break;
-                }
-            }
-            if (only_spaces)
-                continue;
-        }
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
-        /* Tokenize */
-        i = 0;
-        tokens[i] = strtok(line, " \t");
-        while (tokens[i] && i < 99)
-        {
-            i++;
-            tokens[i] = strtok(NULL, " \t");
-        }
+		i = 0;
+		tokens[i] = strtok(line, " \t");
+		while (tokens[i] && i < 99)
+			tokens[++i] = strtok(NULL, " \t");
 
-        if (!tokens[0])
-            continue;
+		if (!tokens[0])
+			continue;
 
-        /* Built-in: exit */
-        if (strcmp(tokens[0], "exit") == 0)
-        {
-            free(line);
-            exit(status);
-        }
+		/* exit built-in */
+		if (strcmp(tokens[0], "exit") == 0)
+		{
+			free(line);
+			exit(last_status);
+		}
 
-        /* Built-in: env */
-        if (strcmp(tokens[0], "env") == 0)
-        {
-            i = 0;
-            while (environ[i])
-            {
-                write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-                write(STDOUT_FILENO, "\n", 1);
-                i++;
-            }
-            status = 0;
-            continue;
-        }
+		/* env built-in */
+		if (strcmp(tokens[0], "env") == 0)
+		{
+			for (i = 0; environ[i]; i++)
+				printf("%s\n", environ[i]);
+			last_status = 0;
+			continue;
+		}
 
-        /* PATH resolution */
-        cmd_path = find_command_in_path(tokens[0]);
-        if (!cmd_path)
-        {
-            write(STDERR_FILENO, tokens[0], strlen(tokens[0]));
-            write(STDERR_FILENO, ": not found\n", 12);
-            status = 127;
-            continue;
-        }
+		/* absolute or relative path */
+		if (tokens[0][0] == '/' || tokens[0][0] == '.')
+			cmd_path = tokens[0];
+		else
+			cmd_path = find_in_path(tokens[0]);
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            free(cmd_path);
-            free(line);
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            execve(cmd_path, tokens, environ);
-            perror(cmd_path);
-            free(cmd_path);
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            waitpid(pid, &status, 0);
-            if (WIFEXITED(status))
-                status = WEXITSTATUS(status);
-        }
+		/* command not found → NO fork */
+		if (!cmd_path)
+		{
+			dprintf(STDERR_FILENO, "%s: %u: %s: not found\n",
+				argv[0], line_number, tokens[0]);
+			last_status = 127;
+			continue;
+		}
 
-        free(cmd_path);
-    }
-
-    free(line);
-    return (0);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			if (execve(cmd_path, tokens, environ) == -1)
+			{
+				perror(argv[0]);
+				exit(2);
+			}
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
+				last_status = WEXITSTATUS(status);
+		}
+	}
 }
 
